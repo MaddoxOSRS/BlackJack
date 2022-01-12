a9a34a61ad4a8a5ac9f9bcbd4d4c7e777e3cd81e
Maddox <95894698+MaddoxOSRS@users.noreply.github.com>
2022-01-12 12:29:55 -0500

    Removed unneeded login in knockout branch

--- src/io/maddox/behaviour/KnockandPick/ActivateKnockout.java
+++ src/io/maddox/behaviour/KnockandPick/ActivateKnockout.java
@@ -7,16 +7,11 @@ import org.powbot.api.rt4.*;
 
 public class ActivateKnockout extends Branch {
 
-    GameObject closedcurtain;
     @Override
     public boolean isValid() {
-        closedcurtain = Objects.stream().at(Configs.curtain).id(Configs.closedCurtain).nearest().first();
         return Configs.house.contains(Npcs.stream().within(Configs.house).id(Configs.thug).nearest().first())
-                && Inventory.stream().name(Configs.food).action("Eat").isNotEmpty()
-                && Configs.house.contains(Players.local()) && !Configs.cantKnock &&
-                closedcurtain.valid() || Players.local().healthPercent() < Configs.toEat || Players.local().healthPercent() < Configs.toEat
-                && !Inventory.stream().name(Configs.food).action("Eat").isEmpty()||
-                Players.local().healthPercent() < Configs.toEat && !Inventory.stream().name(Configs.food).action("Drink").isEmpty() || Inventory.stream().name(Configs.moneyPouch).count() == 28;
+                && Configs.house.contains(Players.local())
+                && !Configs.cantKnock;
     }
 
 }
